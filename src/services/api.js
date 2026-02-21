c6a9ebc8118e9f1155f9f2766114b18ec8e564ce
vinothkumar s <vinothkumar.s@emmvee.in>
2026-02-21 14:56:10 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8001/api",
+  baseURL: "https://leads-backend-kwzh.onrender.com/api",
 });
 
 // Attach token automatically
