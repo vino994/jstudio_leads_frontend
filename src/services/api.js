0d40733cdae7ead5fa03ad3b6550f2e29aedb436
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2026-04-23 09:32:36 +0100

    'ipdate'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: "https://jstudio-leads-backend.onrender.com/api",
 });
 
 export default api;
